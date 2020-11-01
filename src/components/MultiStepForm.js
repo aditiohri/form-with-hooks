8a948b6500cdaf11f4f34bcb31d1a1a6c1ac71d6
Aditi Ohri <didiohri@gmail.com>
2020-10-31 20:52:32 -0500

    prettier working now

--- src/components/MultiStepForm.js
+++ src/components/MultiStepForm.js
@@ -1,78 +1,93 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
+import Form1 from "./Form1";
+import Form2 from "./Form2";
+import Form3 from "./Form3";
+
 const container = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100vh'
-}
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100vh",
+};
 
 const card = {
-    border: '2px dotted green',
-    alignSelf: 'center',
-    padding: '1em'
-}
+  border: "2px dotted green",
+  alignSelf: "center",
+  padding: "1em",
+};
 
 export default function MultiStepForm() {
+  const [options, setOptions] = useState([]);
+  const [textValue, setTextValue] = useState("");
+  const [radioValue, setRadioValue] = useState("");
 
-    const [options, setOptions] = useState([]);
-    const [textValue, setTextValue] = useState("");
-    const [radioValue, setRadioValue] = useState("");
+  const handleOptionAdd = () => {
+    if (textValue.trim().length === 0) return;
+    setTextValue("");
+    setOptions([...options, { label: textValue, value: textValue }]);
+  };
 
-    const handleOptionAdd = () => {
-        if (textValue.trim().length === 0) return;
-        setTextValue("");
-        setOptions([
-            ...options,
-            { label: textValue, value: textValue},
-        ])
-    }
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
 
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+  return (
+    <div style={container}>
+      <div style={card}>
+        <h1>A Form For Fun</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Form1 
+            ref={register}
+            setTextValue={setTextValue}
+            // prop to change step value
+          />
+          <Form2
+            ref={register}
+            setTextValue={setTextValue}
+          />
+          <Form3
+            ref={register}
+            setTextValue={setTextValue}
+          />
 
+          <h3>
+            <label>Name</label>
+          </h3>
 
-    return (
-        <div style={container}>
-            <div style={card}>
+          <input name="firstName" ref={register} />
 
-                <h1>A Form For Fun</h1>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    
-                    <h3><label>Name</label></h3>
-                    <input name="firstName" ref={register} />
-                    
-                    <h3><label>What ice cream flavors would you like?</label></h3>
-                    <input 
-                        type="text"
-                        value={textValue}
-                        onChange={(e) => setTextValue(e.target.value)}
-                    />
-                    <button onClick={handleOptionAdd}>Add</button>
+          <h3>
+            <label>What ice cream flavors would you like?</label>
+          </h3>
+          <input
+            type="text"
+            value={textValue}
+            onChange={(e) => setTextValue(e.target.value)}
+          />
+          <button onClick={handleOptionAdd}>Add</button>
 
-                    <div>
-                        {options.map((option) => (
-                            <div>
-                                <input 
-                                    type="radio"
-                                    name="iceCream"
-                                    value={option.value}
-                                    checked={radioValue === option.value}
-                                    onChange={(e) => setRadioValue(e.target.value)}
-                                    ref={register}
-                                />
-                                <label>{option.label}</label>
-                            </div>
-                        ))}
-                    </div>
+          <div>
+            {options.map((option) => (
+              <div>
+                <input
+                  type="radio"
+                  name="iceCream"
+                  value={option.value}
+                  checked={radioValue === option.value}
+                  onChange={(e) => setRadioValue(e.target.value)}
+                  ref={register}
+                />
+                <label>{option.label}</label>
+              </div>
+            ))}
+          </div>
 
-                    <div>
-                        <button type="submit">Submit</button>
-                    </div>
-
-                </form>
-            </div>
-        </div>
-    )
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
