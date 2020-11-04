ca35d0226fdfb27974fbcc66d44928287746f84d
Aditi Ohri <didiohri@gmail.com>
2020-11-04 17:59:23 -0600

    show user's answers on review page

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { useStateMachine } from "little-state-machine";
+import updateAction from "./updateAction";
+
+
 
 export default function Review({ prevPage, reStart }) {
+  const { state } = useStateMachine(updateAction);
   return (
     <div>
       <h3>REVIEW</h3>
       <div>
+        <p>
+          Your favorite color: {state.form.color}
+          </p>
+        <p>
+          Your favorite animal: 
+          {state.form.animal}
+          </p>
+        <p>
+          Your favorite body of water: 
+          {state.form.water}
+          </p>
+          
         <button onClick={prevPage}>Back</button>
         <button onClick={reStart}>START OVER</button>
         <button type="submit">Submit</button>
